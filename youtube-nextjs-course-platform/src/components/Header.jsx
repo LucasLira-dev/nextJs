a87a003782169ca2c928076cf6dc7359c41bef8c
lucas mendes <lucasmndes.dev@gmail.com>
2025-04-15 11:42:56 -0300

    header responsivo

--- youtube-nextjs-course-platform/src/components/Header.jsx
+++ youtube-nextjs-course-platform/src/components/Header.jsx
@@ -1,38 +1,50 @@
 'use client'
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { MdOpenInNew } from "react-icons/md";
+import { MdMenu, MdOpenInNew } from "react-icons/md";
 
 export function Header(){
 
     const currentPath= usePathname()
+    
 
     return(
        <header>
             <nav 
-            className="flex items-center justify-center bg-[#009688] py-4">
+            className="flex items-center gap-6 justify-start md:justify-center bg-[#009688] py-2 px-6 md:py-4">
+
+                <button className="md:hidden text-[var(--color-text)] cursor-pointer hover:opacity-80">
+                    <MdMenu size={24}/>
+                </button>
+                
                 <ul
                 className="flex gap-2 text-[var(--color-text)] text-lg items-center">
                     <li className='my-2 hover:no-underline'>
                         <Link href='/' className="border-2 rounded-md py-2 px-1 font-bold hover:no-underline"> CODARSE </Link>
                     </li>
-                    <li>
+                    <li
+                    className="hidden sm:block">
                         <Link href='/'
                         data-active={currentPath==='/'}
                         className='data-[active=true]:underline'> Página inicial </Link>
                     </li>
-                    <li>
+                    <li
+                    className="hidden sm:block">
                         <Link href='/cursos'
                         data-active={currentPath==='/cursos'}
                         className='data-[active=true]:underline'> Cursos </Link>
                     </li>
-                    <li>
+                    <li
+                    className="hidden sm:block">
                         <Link href='https://blog.codarse.com' target='_blank' className='flex gap-1 items-center'> 
                             Blog 
                             <MdOpenInNew className="inline-block ml-1" size={16} color="#fff" />
                         </Link>
                     </li>
                 </ul>
+
+                <h1
+                className="sm:hidden"> CodarSe - Página inicial </h1>
             </nav>
        </header>
     )
